9791c85e3f3d936503b857a9c5aa5754cf1a7e61
Evie-Skinner18 <eskinner@spartaglobal.com>
2019-10-12 18:32:07 +0100

    Added code to start the node server and the root route, aswell as settings for public dir and EJS, to app.js. Added home.ejs to views

--- post_requests/app.js
+++ post_requests/app.js
@@ -1,3 +1,18 @@
+const express = require('express');
+const app = express();
+
+// tell express to use public dir for styles
+app.use(express.static('public'));
+// tell it to use EJS for views
+app.set('view engine', 'ejs');
+
+// root route
+app.get('/', function(request, response){
+  response.render('home');
+});
+
+
+
 //start the node server
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
