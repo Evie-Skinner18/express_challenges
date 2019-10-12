529f6b57feb29ae345cad43241bea659a19e0c58
Evie-Skinner18 <eskinner@spartaglobal.com>
2019-10-12 19:25:42 +0100

    Moved friends array out of friends GET function so it's now in scope for all the routes. Made a var newFriend which will hold the value of the new friend tha user enters in the form,and using body parser we will extract this. Now you can see your new friend being added in a new <li> when you click add friend

--- post_requests/app.js
+++ post_requests/app.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+
+// tell expressto use body parser so we can extract data from the body of the request
+app.use(bodyParser.urlencoded({extended: true}));
 
 // tell express to use public dir for styles
 app.use(express.static('public'));
 // tell it to use EJS for views
 app.set('view engine', 'ejs');
+
+// friends array which is like a mini non-persisting DB for now :P
+let friends = ['Dibby', 'Adam', 'Maja', 'Patrick', 'Foofoo', 'Bikesh'];
+
 
 // root route
 app.get('/', function(request, response){
@@ -12,17 +21,21 @@ app.get('/', function(request, response){
 });
 
 app.get('/friends', function(request, response){
-  let friends = ['Dibby', 'Adam', 'Maja', 'Patrick', 'Foofoo', 'Bikesh'];
   // the object we send specifies a property, friends, that is populated by an array var friends that
-  // we've just made
+  // we've made. This array is up there so it's in scope to all the route functions
   response.render('friends', {friends: friends});
 });
 
 app.post('/addfriend', function(request,response){
   // print everything in the body of the request. BodyPArser package tells Express to turn the body
-  // of the request into a JS bject that you can see
-  console.log(request.body);
-  response.send('You have reached the POST route!');
+  // of the request into a JS bject that you can see. Anytime we are taking input from the user in a form,
+  // we will need body-parser so we can extract the data from the body on the server side
+  // save the new friend the user has added using the form to a var
+  let newFriend = request.body.newfriend;
+  // push the new friend into the friends array defined up above
+  friends.push(newFriend);
+  // view all the friends inc new one
+  response.render('friends', {friends: friends});
 });
 
 
