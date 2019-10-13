53fec3c879da4b3c34f74225b079b224d7e07856
Evie-Skinner18 <eskinner@spartaglobal.com>
2019-10-13 17:29:05 +0100

    Learning how to make requests to APIs in Node using a package called request. Http client

--- working_with_apis/app.js
+++ working_with_apis/app.js
@@ -1,3 +1,18 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+// bodyParser
+
+// use public for stylesheets
+app.use('public');
+// use EJS for views
+
+
+// 
+
+
+
+
 //start the node server
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
