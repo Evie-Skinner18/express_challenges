447cf1b6daec8a2c713df265d153cff01201c7e8
Evie-Skinner18 <eskinner@spartaglobal.com>
2019-10-13 17:42:30 +0100

    Added a simple GET request to get a random beer from the Punk API

--- working_with_apis/app.js
+++ working_with_apis/app.js
@@ -1,11 +1,33 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
+const request = require('request');
+
+// make a simple GET request to the Punk API. To do anything with the info that comes back,
+// we need callback function
+request('https://api.punkapi.com/v2/beers/random', function(error, response, body){
+  // how are you going to handle the different types of response?
+    if(error){
+      console.log('There is an error:', error);
+    }
+    else if(!error && response.statusCode == 200){
+      console.log(response.statusCode);
+      console.log(body);  
+    }
+    else{
+      console.log(response.statusCode);
+    }
+});
+
+
+
+
+
+// const express = require('express');
+// const app = express();
+// const bodyParser = require('body-parser');
 // bodyParser
 
 // use public for stylesheets
-app.use('public');
-// use EJS for views
+// app.use('public');
+// use EJS for vieŸws
 
 
 // 
@@ -14,8 +36,8 @@ app.use('public');
 
 
 //start the node server
-var port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Server Has Started!');
-});
+// var port = process.env.PORT || 3000;
+// app.listen(port, function () {
+//   console.log('Server Has Started!');
+// });
 
