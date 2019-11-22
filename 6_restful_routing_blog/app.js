cdcfa3ee79652be9d410f26ac8c79737583ac0aa
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2019-11-22 12:39:18 +0000

    Made the index GET all blogs route

--- 6_restful_routing_blog/app.js
+++ 6_restful_routing_blog/app.js
@@ -3,22 +3,40 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27107/blogApp', {useNewUrlParser: true, useUnifiedTopology: true});
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect('mongodb://localhost:27017/blogApp', {useNewUrlParser: true, useUnifiedTopology: true});
+// mongoose.connect('mongodb://localhost:27017/yelpCamp', {useNewUrlParser: true, useUnifiedTopology: true});
+
 const blogSchema = new mongoose.Schema({
     title: String,
     image: String,
     body: String,
-    dateCreated: Date
+    dateCreated: {type: Date, default: Date.now}
 });
 const Blog = mongoose.model('Blog', blogSchema);
+// Blog.create({
+//     title: 'Why I Love Real Ale',
+//     image: 'https://www.stroudbrewery.co.uk/wp-content/uploads/2018/11/Bottle-Hop-Drop.jpg',
+//     body: 'I love real ale because it\'s tasty and fun to drink. Every beer is different and special.'
+// });
 
-
-app.set('view engine', 'ejs');
-app.use(express.static, ('public'));
-app.use(bodyParser.urlencoded, ({extended: true}));
 
 app.get('/', (req, res)=> {
-    res.send('ROOT ROUTE');
+    res.redirect('/blogs');
+});
+
+app.get('/blogs', (req, res)=> {
+    Blog.find({}, (err, foundBlogs)=> {
+        if(err){
+            res.send('Sorry couldn\'t find any blogs!');
+        }
+        else{
+            res.render('index', {blogs: foundBlogs});
+        }
+    });
 });
 
 
